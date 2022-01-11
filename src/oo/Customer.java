db3832af17d5b406bcc1d033c211409559c6d524
NguyenTruongSon0101 <sonn6272@gmail.com>
2022-01-11 08:48:45 +0700

    nhan vien + khach hang

---
+++ src/oo/Customer.java
@@ -0,0 +1,16 @@
+package oo;
+
+public class Customer {
+    // data
+    String tenKH = "NguyenTruongSon";
+    int sDT = 12345;
+    String diaChi = "PhamVanDong";
+
+    //function
+    void inThongTin(){
+        System.out.println("Thong tin khach hang");
+        System.out.println("Ten khach hang: "+tenKH);
+        System.out.println("So dien thoai:" +sDT);
+        System.out.println("Dia chi: "+diaChi);
+    }
+}
---
+++ src/oo/Employee.java
@@ -0,0 +1,22 @@
+package oo;
+
+public class Employee {
+    // data
+    int maNV = 001;
+    String tenNV = "Nguyen Van A";
+    String congTac = "chi nhanh Thu Duc";
+    int luongCoBan = 5000000;
+
+    //function
+    void inThongTin(){
+        System.out.println("thong tin nhan vien");
+        System.out.println("Ten nhan vien: "+tenNV);
+        System.out.println("Ma nhan vien: "+maNV);
+        System.out.println("Noi cong tac: "+congTac);
+    }
+
+    void tinhLuong(int heSoLuong){
+        int thucLanh = heSoLuong * luongCoBan;
+        System.out.println("Luong nhan vien: "+thucLanh);
+    }
+}
