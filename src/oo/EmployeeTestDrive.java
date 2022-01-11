863270ecb1d94b057a71d0d94f82d31bacb53d7c
NguyenTruongSon0101 <sonn6272@gmail.com>
2022-01-11 08:32:01 +0700

    phat trien khach hang nhan vien

---
+++ src/oo/EmployeeTestDrive.java
@@ -0,0 +1,13 @@
+package oo;
+
+public class EmployeeTestDrive {
+    public static void main(String[] args) {
+        Employee employee;
+        employee = new Employee();
+
+        employee.inThongTin();
+
+        employee.tinhLuong(2);
+    }
+}
+
